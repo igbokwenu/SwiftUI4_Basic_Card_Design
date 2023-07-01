a3cc374158bfa2658eb7cace9f75060707808169
Increase MacOS <okechukwu@skylinefuturistic.com>
2023-07-01 18:39:49 +0100

    Initial Commit

---
+++ suift_ui_course/suift_ui_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  suift_ui_courseApp.swift
+//  suift_ui_course
+//
+//  Created by Increase Okechukwu Divine-Wisdom on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct suift_ui_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
